fecf4ce48a84781802e7d7f703d7a5a5ae81b434
joao-pedro213 <joao.capobiango213@gmail.com>
2020-08-16 14:41:41 -0300

    Estilizando o app.

--- src/components/readOnlyInput/ReadOnlyInput.js
+++ src/components/readOnlyInput/ReadOnlyInput.js
@@ -5,14 +5,23 @@ export default class ReadOnlyInput extends Component {
   render() {
     const { inputLabel, value, textColor } = this.props;
     return (
-      <div className={`input-field ${css.readOnlyInput}`}>
+      <div
+        style={{ borderColor: textColor }}
+        className={`input-field ${css.readOnlyInput} ${css.border}`}
+      >
         <input
-          style={{ color: textColor, fontWeight: 'bold' }}
+          style={{
+            color: textColor,
+            fontWeight: 'bold',
+            borderColor: textColor,
+          }}
           value={value}
           type="text"
           readOnly
         />
-        <label className={'active'}>{inputLabel}</label>
+        <label style={{ color: textColor }} className={'active'}>
+          {inputLabel}
+        </label>
       </div>
     );
   }

194be271bb7ee006aee55ed7bf31885eff8a4bb0
joao-pedro213 <joao.capobiango213@gmail.com>
2020-08-16 02:17:51 -0300

    Implementando as regras de calculo de desconto de INSS e IRPF.

--- src/App.js
+++ src/App.js
@@ -2,31 +2,74 @@ import React, { Component } from 'react';
 import Header from './components/header/Header';
 import SalaryInput from './components/salaryInput/SalaryInput';
 import ReadOnlyInput from './components/readOnlyInput/ReadOnlyInput';
+import { calculateSalaryFrom } from './controller/salaryController.js';
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fullSalary: '',
+      baseINSS: 0,
+      baseIRPF: 0,
+      discountINSS: 0,
+      discountIRPF: 0,
+      netSalary: 0,
+    };
+  }
+
+  handleInputChangeValue = (value) => {
+    const typedSalary = value === '' ? 0 : value;
+    const {
+      baseINSS,
+      baseIRPF,
+      discountINSS,
+      discountIRPF,
+      netSalary,
+    } = calculateSalaryFrom(typedSalary);
+    this.setState({
+      fullSalary: typedSalary,
+      baseINSS,
+      baseIRPF,
+      discountINSS,
+      discountIRPF,
+      netSalary,
+    });
+  };
+
   render() {
+    const {
+      fullSalary,
+      baseINSS,
+      baseIRPF,
+      discountINSS,
+      discountIRPF,
+      netSalary,
+    } = this.state;
     return (
       <div className="container">
         <Header />
-        <SalaryInput />
+        <SalaryInput
+          inputValue={fullSalary}
+          onChangeInput={this.handleInputChangeValue}
+        />
         <div style={styles.flexRow}>
-          <ReadOnlyInput value={'Test'} inputLabel={'Base INSS:'} />
+          <ReadOnlyInput value={baseINSS} inputLabel={'Base INSS:'} />
           <ReadOnlyInput
             textColor={'#e67e22'}
-            value={'Test'}
+            value={discountINSS}
             inputLabel={'Desconto INSS:'}
           />
-          <ReadOnlyInput value={'Test'} inputLabel={'Base IRPF:'} />
+          <ReadOnlyInput value={baseIRPF} inputLabel={'Base IRPF:'} />
           <ReadOnlyInput
             textColor={'#c0392b'}
-            value={'Test'}
+            value={discountIRPF}
             inputLabel={'Desconto IRPF:'}
           />
         </div>
         <div style={styles.flexRow}>
           <ReadOnlyInput
             textColor={'#16a085'}
-            value={'Test'}
+            value={netSalary}
             inputLabel={'Salário líquido:'}
           />
         </div>
--- src/components/salaryInput/SalaryInput.js
+++ src/components/salaryInput/SalaryInput.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import css from './salaryInput.module.css';
 
 export default class SalaryInput extends Component {
+  handleInputValue = (event) => {
+    const typedSalary = event.target.value;
+    this.props.onChangeInput(typedSalary);
+  };
+
   render() {
     return (
       <div className={`input-field ${css.salaryInput}`}>
         <input
           placeholder="Digite um valor para calcular o salário bruto"
           type="number"
+          onChange={this.handleInputValue}
         />
         <label className="active">Salário bruto</label>
       </div>
